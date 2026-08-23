44b36d3e77c87ae09f5e9ddf0be8c8e2a19aa3b9
waheeb4 <adhamwaheeb4444@gmail.com>
2026-08-23 15:23:42 +0300

    Contact section created and added in the Base Layout with corresponding assets & font configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [
+    {
+      name: "Special Gothic Expanded One",
+      cssVariable: "--font-special-gothic-expanded-one",
+      provider: fontProviders.google(),
+    },
+  ],
+});
